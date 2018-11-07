77e4b20e83cbfce7a887fd10273645ab76260fdd
Lenny Knoop <lennartpknoop@gmail.com>
2018-11-07 18:13:37 +0100

    postRequest to change the meters is now working :D

--- apiPostRequest.rb
+++ apiPostRequest.rb
@@ -19,9 +19,23 @@ body = {
   ]
 }
 
-Net::HTTP.start(uri.host, uri.port) do |http|
-  res = Net::HTTP.post_form(uri, 'Command' => "CTRatioSet", "Settings" => ["CTRatioSet" => {"Access_Password" => "00000000", "CT_Ratio" => 800}])
-  puts res.body
+https = Net::HTTP.new(uri.host, uri.port)
+https.use_ssl = true
 
-  response = http.request request # Net::HTTPResponse object
-end
+req = Net::HTTP::Post.new(uri.path.concat("?meter=66&key=ZGZsMjZhRTJWVDo2NTI0MDkxMQ"))
+req['Content-Type'] = 'application/json'
+# req['charset'] = 'UTF-8'
+req.body = {
+  "Command": "CTRatioSet",
+  "Settings": [
+    {
+      "CTRatioSet": {
+        "Access_Password": "00000000",
+        "CT_Ratio": 800
+      }
+    }
+  ]
+}.to_json
+res = https.request(req)
+
+p res
